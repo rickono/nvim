6309da1cbd8bb363467c9ea4d49c4802857b71fe
Ryuta Ono <rick.ono@robinhood.com>
2023-06-07 15:56:21 -0400

    Use LuaSnip version 1

--- lua/rickono/packer.lua
+++ lua/rickono/packer.lua
@@ -47,7 +47,7 @@ return require('packer').startup(function(use)
     use({
         "L3MON4D3/LuaSnip",
         -- follow latest release.
-        tag = "v<CurrentMajor>.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+        tag = "v1.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
         -- install jsregexp (optional!:).
         run = "make install_jsregexp"
     })
